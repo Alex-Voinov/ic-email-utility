e1d66854bde7db2eb3afc7ac841f88b2e25b0c6d
Alex Frost <VoinovAlex1900@yandex.ru>
2025-01-20 22:13:50 +0300

    test

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const mailer = require('./mailer'); 
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -13,10 +14,22 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Ответ на /api/hi
-app.get('/api/hi', (req, res) => {
-  res.send('Привет');
-});
+app.post('/api/sendRequest', async (req, res) => {
+    try {
+      const { text } = req.body;
+  
+      // Проверка на обязательные поля
+      if ( !text) {
+        return res.status(400).send('Недостающие обязательные данные');
+      }
+  
+      // Отправка почты через mailer
+      const result = await mailer.sendEmail(text);
+      res.status(200).send(result);
+    } catch (error) {
+      res.status(500).send('Ошибка при отправке почты: ' + error.message);
+    }
+  });
 
 // Запуск сервера
 app.listen(port, () => {
